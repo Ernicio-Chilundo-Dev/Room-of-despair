b08ae884a6fa67ba0d0e6133d240e0a5ef0b0790
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2024-11-02 23:35:58 +0200

    Array com perguntas e respostas criada com sucesso

--- script.js
+++ script.js
@@ -1,47 +1,24 @@
-('use strict')
+// Perguntas e respostas do jogo
 
-let banco = [
-    { db: "parametros sao valores ou variaveis que sao passados para funcoes ou procedimentos para definir seu comportamento ou influenciar seus resultados." },
-    { db: "argumentos reais ou parametros sao valores reais passados para a funcao quando ela e chamada." },
-    { db: " parametros formais sao definidos na declaracao da funcao e atuam como variaveis locais dentro do corpo da funcao." }
+const questions = [
+    {
+        question: "Qual e a capital de Mocambique?",
+        answers:['Maputo','Beira','Nampula','Pemba'],
+        correct: 'Maputo'
+    },
+    {
+        question: "Qual e a linguagem de programacao principal para desenvolvimento web?",
+        answers:['Python','JavaScript','C++','Java'],
+        correct: 'JavaScript'
+    },
+    {
+        question: "Quantos planetas existem no sistema solar?",
+        answers:['7','8','9','10'],
+        correct: '8'
+    },
+    {
+        question: "Quantos continentes existem no mundo?",
+        answers:['5','6','7','8'],
+        correct: '7'
+    }
 ]
-
-const question = document.getElementById('question');
-const roomDespair = document.getElementById('roomDespair');
-const btnApagar = document.getElementById('btnApagar');
-
-function createElement(text){
-    const paragrafo = document.createElement('p')
-    paragrafo.setAttribute("class","class");
-    paragrafo.textContent = text
-    return paragrafo
-}
-
-function apagar(){
-    if(roomDespair){
-        roomDespair.innerHTML = '';
-    }
-}
-
-const from = createElement('Ola! Seja bem vido')
-roomDespair.appendChild(from)
-
-function content(evento){
-if(evento.key == 'Enter'){
-    const enviar = evento.textContent = evento.target.value
-    from.textContent ='';
-    if(enviar != ""){
-        roomDespair.appendChild(createElement(enviar))
-        const dados = banco.find(i => i.db.includes(enviar))
-        if(dados){
-            roomDespair.appendChild(createElement(dados.db))
-        }
-        evento.target.value='';
-    }else{
-        roomDespair.appendChild(createElement('Erro: O campo de texto encontra-se vazio!'))
-    }
-}
-}
-
-btnApagar.addEventListener('click', apagar);
-question.addEventListener("keypress", content)
